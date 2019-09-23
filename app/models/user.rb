8e35bf644669058c63b4374b9c9fdf69dfa49e1b
gonjavi <javiercaliescali@gmail.com>
2019-09-23 13:14:49 -0500

    adding email validator class, to validate email on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, uniqueness: true
-    validates :email, uniqueness: true, presence: true
+    validates :email, uniqueness: true, presence: true, email: true
     validates :name, presence: {string: true}, length:{minimum: 4}
     has_many :posts
 end
