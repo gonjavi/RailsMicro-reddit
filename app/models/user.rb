bc1e6f6f68a5876c1465a37b63fc3eea39056130
gonjavi <javiercaliescali@gmail.com>
2019-09-27 14:38:32 -0500

    adding presence true to username, and name minimum length 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    validates :username, uniqueness: true
+    validates :username, uniqueness: true, presence: true
     validates :email, uniqueness: true, presence: true, email: true
-    validates :name, presence: {string: true}, length:{minimum: 5}
+    validates :name, presence: {string: true}, length:{minimum: 2}
     has_many :posts
     has_many :comments
 end
